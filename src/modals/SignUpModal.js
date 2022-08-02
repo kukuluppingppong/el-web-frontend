495c324215028ccb2498149e00b52ad547e5b9a5
jeonchaerin <chaerin0411@naver.com>
2022-08-02 15:08:08 +0900

    회원가입 유효성 검사 수정

--- src/modals/SignUpModal.js
+++ src/modals/SignUpModal.js
@@ -44,15 +44,17 @@ const SignUpModal = ({ show, onHide }) => {
                     <Form onSubmit={handleSubmit(onSubmit)} method="post">
                         <Form.Group>
                             <Form.Label>아이디</Form.Label>
-                            <Form.Control type="text" name="id" placeholder="아이디 입력" autoFocus className="mb-3 rounded-pill" {...register("id", { required: true, minLength: 6 })} />
+                            <Form.Control type="text" name="id" placeholder="아이디 입력" autoFocus className="mb-3 rounded-pill"
+                                {...register("id", { required: true, maxLength: 20, pattern: { value: /^[a-z]+[a-z0-9]{5,19}$/g, message: "잘못된 아이디 형식입니다." } })} />
                             {errors.id && errors.id.type === "required"
                                 && <p> This id field is required</p>}
                             {errors.id && errors.id.type === "minLength"
-                                && <p> id must have at least 6 characters</p>}
+                                && <p> Your input exceed maximum length</p>}
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>비밀번호</Form.Label>
-                            <Form.Control type="password" name="password" placeholder="비밀번호 입력" className="mb-3 rounded-pill" {...register("password", { required: true, minLength: 8 })} />
+                            <Form.Control type="password" name="password" placeholder="비밀번호 입력" className="mb-3 rounded-pill"
+                                {...register("password", { required: true, minLength: 8, pattern: /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/ })} />
                             {errors.password && errors.password.type === "required"
                                 && <p> This password field is required</p>}
                             {errors.password && errors.password.type === "minLength"
@@ -60,15 +62,19 @@ const SignUpModal = ({ show, onHide }) => {
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>비밀번호 확인</Form.Label>
-                            <Form.Control type="password" name="passwordConfirm" placeholder="비밀번호 재입력" className="mb-3 rounded-pill" {...register("passwordConfirm", { required: true, validate: (value) => value === password.current })} />
+                            <Form.Control type="password" name="passwordConfirm" placeholder="비밀번호 재입력" className="mb-3 rounded-pill"
+                                {...register("passwordConfirm", { required: true, minLength: 8, pattern: /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/, validate: (value) => value === password.current })} />
                             {errors.passwordConfirm && errors.passwordConfirm.type === "required"
                                 && <p> This password confirm field is required</p>}
+                            {errors.passwordConfirm && errors.passwordConfirm.type === "minLength"
+                                && <p> Password confirm must have at least 8 characters</p>}
                             {errors.passwordConfirm && errors.passwordConfirm.type === "validate"
                                 && <p>The passwords do not match</p>}
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>이름</Form.Label>
-                            <Form.Control type="text" name="name" placeholder="이름 입력" className="mb-3 rounded-pill" {...register("name", { required: true, maxLength: 10 })} />
+                            <Form.Control type="text" name="name" placeholder="이름 입력" className="mb-3 rounded-pill"
+                                {...register("name", { required: true, maxLength: 10 })} />
                             {errors.name && errors.name.type === "required"
                                 && <p> This name field is required</p>}
                             {errors.name && errors.name.type === "maxLength"
@@ -76,17 +82,20 @@ const SignUpModal = ({ show, onHide }) => {
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>전화번호</Form.Label>
-                            <Form.Control type="tel" name="phone" placeholder="전화번호 입력" className="mb-3 rounded-pill" {...register("phone", { required: true })} />
+                            <Form.Control type="tel" name="phone" placeholder="전화번호 입력" className="mb-3 rounded-pill"
+                                {...register("phone", { required: true })} />
                             {errors.email && <p>This phone field is required</p>}
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>이메일</Form.Label>
-                            <Form.Control type="email" name="email" placeholder="이메일 입력" className="mb-3 rounded-pill" {...register("email", { required: true, pattern: /^\S+@\S+$/i })} />
+                            <Form.Control type="email" name="email" placeholder="이메일 입력" className="mb-3 rounded-pill"
+                                {...register("email", { required: true, pattern: /^\S+@\S+$/i })} />
                             {errors.email && <p>This email field is required</p>}
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>헬스장 위치</Form.Label>
-                            <Form.Control type="search" name="addr" placeholder="헬스장 위치 입력" className="mb-3 rounded-pill" {...register("addr", { required: true })} />
+                            <Form.Control type="search" name="addr" placeholder="헬스장 위치 입력" className="mb-3 rounded-pill"
+                                {...register("addr", { required: true })} />
                             {errors.email && <p>This addr field is required</p>}
                         </Form.Group>
                         <Form.Group className="d-grid gap-2">
