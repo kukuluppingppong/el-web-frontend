d8f23d78328ad92d4d5136a7a2e1b130e729aec6
jeonchaerin <chaerin0411@naver.com>
2022-08-02 23:30:12 +0900

    라우트 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
-import React, { useEffect, useState } from 'react';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import axios from 'axios';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 import { Container } from "react-bootstrap";
 import Layout from "./layouts/Layout";
 import MemberList from './components/menber/MemberList';
+import MemberWrite from './components/menber/MemberWrite';
+import MemberView from './components/menber/MemberView';
+
 
 function App(props) {
 
   return (
     <Layout>
-      <Container><MemberList /></Container>
-      <Container style={{ minHeight: "80vh" }}>회원관리</Container>
+      <Container style={{ minHeight: "80vh" }}>
+        <MemberList />
+        {/* <BrowserRouter>
+          <Routes>
+            <Route exact path='/' element={<MemberList />}></Route>
+            <Route exact path='/memberWrite' element={<MemberWrite />}></Route>
+            <Route exact path='/memberView/:id' element={<MemberView />}></Route>
+          </Routes>
+        </BrowserRouter> */}
+      </Container>
     </Layout>
   );
 }
