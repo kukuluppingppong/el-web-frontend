17d92db9ef6da9df82c1834e8ef1cd0dfd4b5c40
jeonchaerin <chaerin0411@naver.com>
2022-08-05 01:23:17 +0900

    QnA 리스트 테스트

--- client/src/components/QnA/QnAList.js
+++ client/src/components/QnA/QnAList.js
@@ -1,11 +1,62 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-const QnAList = (props) => {
+function createData(
+    no,
+    title,
+    writer,
+    regDate,
+    answer,
+) {
+    return { no, title, writer, regDate, answer };
+}
+
+const rows = [
+    createData(1, '제목입니다.', '관리자', '2022-08-05', 'O'),
+    createData(1, '제목입니다.', '관리자', '2022-08-05', 'O'),
+    createData(1, '제목입니다.', '관리자', '2022-08-05', 'O'),
+    createData(1, '제목입니다.', '관리자', '2022-08-05', 'O'),
+    createData(1, '제목입니다.', '관리자', '2022-08-05', 'O'),
+];
+
+const QnAList = () => {
     return (
-        <div className="QnAView">
-
-        </div>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>번호</TableCell>
+                        <TableCell align="right">제목</TableCell>
+                        <TableCell align="right">작성자</TableCell>
+                        <TableCell align="right">등록일</TableCell>
+                        <TableCell align="right">답변</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow
+                            key={row.no}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.no}
+                            </TableCell>
+                            <TableCell align="right">{row.title}</TableCell>
+                            <TableCell align="right">{row.writer}</TableCell>
+                            <TableCell align="right">{row.regDate}</TableCell>
+                            <TableCell align="right">{row.answer}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
