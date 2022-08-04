d1eb06677a03d643e94e06bfbd36fdbc1680d049
jeonchaerin <chaerin0411@naver.com>
2022-08-04 15:06:39 +0900

    알림 페이지 추가,피드백 운동 및 식단 페이지 추가

---
+++ client/src/components/notification/Notification.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+
+export const Notification = (props) => {
+    return (
+        <TableRow>
+            <TableCell align="center">{this.props.id}</TableCell>
+            <TableCell align="center">
+                <NavLink to={"/notice/" + this.props.id}>
+                    {this.props.title}
+                </NavLink>
+            </TableCell>
+            <TableCell align="center">{this.props.date}</TableCell>
+        </TableRow>
+    )
+}
