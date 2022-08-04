6b32f92705f4903c2172c3700a8ab0f98c51fe03
jeonchaerin <chaerin0411@naver.com>
2022-08-04 15:53:23 +0900

    피드백 운동 및 식단 페이지 수정

--- client/src/components/feedback/FeedbackDiet.js
+++ client/src/components/feedback/FeedbackDiet.js
@@ -20,18 +20,45 @@ const useStyles = makeStyles(theme => ({
 const QnADiet = (props) => {
     const classes = useStyles();
 
+    const [feedbackDiet, setFeedbackDiet] = useState({
+        feedback: '',
+    })
+    const inputChange = (e) => {
+        console.log(e.target.name)
+        console.log(e.target.value)
+        setFeedbackDiet({
+            ...feedbackDiet,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
-        <div className={classes.root}>
-            <ExpansionPanel className={classes.panel}>
-                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography>질문: {props.question}</Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                    <Typography>
-                        답변: {props.answer}
-                    </Typography>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
+        <div>
+            <h1>피드백 운동</h1>
+            <div className='feedbackDiet'>
+                <ExpansionPanel className={classes.panel}>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                        <Typography>사진: {props.image}</Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <Typography>
+                            피드백: {props.feedback}
+                        </Typography>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+
+                <form method='post' action='/api/feedbackDiet'>
+                    <div class="form-group">
+                        <label for="feedback" class="form-label mt-4">피드백 입력</label>
+                        <textarea class="form-control" id="feedback" name='feedback' placeholder='피드백 입력' onChange={inputChange}
+                            className='write_feedback' />
+                    </div>
+                    {/* <input type='text' value={sessionStorage.getItem('id')} placeholder='id 입력' name='id' onChange={inputChange}
+                        className='write_writer_id' /> */}
+                    <input type='submit' value='입력' onClick={() => document.location.href = './FeedbackDiet'} className='feedback_submit' />
+                </form>
+
+            </div>
         </div>
     )
 }
--- client/src/components/feedback/FeedbackWorkout.js
+++ client/src/components/feedback/FeedbackWorkout.js
@@ -20,19 +20,47 @@ const useStyles = makeStyles(theme => ({
 const QnAWorkout = (props) => {
     const classes = useStyles();
 
+    const [feedbackWorkout, setFeedbackWorkout] = useState({
+        feedback: '',
+    })
+    const inputChange = (e) => {
+        console.log(e.target.name)
+        console.log(e.target.value)
+        setFeedbackWorkout({
+            ...feedbackWorkout,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
-        <div className={classes.root}>
-            <ExpansionPanel className={classes.panel}>
-                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography>질문: {props.question}</Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                    <Typography>
-                        답변: {props.answer}
-                    </Typography>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
+        <div>
+            <h1>피드백 운동</h1>
+            <div className='feedbackWorkout'>
+                <ExpansionPanel className={classes.panel}>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                        <Typography>동영상: {props.video}</Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <Typography>
+                            피드백: {props.feedback}
+                        </Typography>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+
+                <form method='post' action='/api/feedbackWorkout'>
+                    <div class="form-group">
+                        <label for="feedback" class="form-label mt-4">피드백 입력</label>
+                        <textarea class="form-control" id="feedback" name='feedback' placeholder='피드백 입력' onChange={inputChange}
+                            className='write_feedback' />
+                    </div>
+                    {/* <input type='text' value={sessionStorage.getItem('id')} placeholder='id 입력' name='id' onChange={inputChange}
+                        className='write_writer_id' /> */}
+                    <input type='submit' value='입력' onClick={() => document.location.href = './FeedbackWorkout'} className='feedback_submit' />
+                </form>
+
+            </div>
         </div>
+
     )
 }
 
