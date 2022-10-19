9a8f001a05c359f95a4af9f94f929f0b6a186e60
jeonchaerin <chaerin0411@naver.com>
2022-10-19 22:17:04 +0900

    feat: Main 페이지 Carousel 추가

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,91 +1,60 @@
 import React from 'react'
+import Slider from "react-slick"
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import data from '../data/data.json'
+
 
 const Main = () => {
+
+    const settings = {
+        dots: true, // 캐러셀이미지가 몇번째인지 알려주는 점을 보여줄지 정한다.
+        infinite: true, // loop를 만들지(마지막 이미지-처음 이미지-중간 이미지들-마지막 이미지)
+        speed: 500, // 애미메이션의 속도, 단위는 milliseconds
+        slidesToShow: 1, // 한번에 몇개의 슬라이드를 보여줄 지
+        slidesToScroll: 1 // 한번 스크롤시 몇장의 슬라이드를 넘길지
+    };
+
+    const workoutList = data.workoutList.map((data, index) => (
+        <div key={index} className="items">
+            <a href="#"></a>
+            <div className="item">
+                <p className="title">{data.title}</p>
+                <p className="comment">{data.comment}</p>
+            </div>
+        </div>
+    ))
+
+    const dietList = data.dietList.map((data, index) => (
+        <div key={index} className="items">
+            <a href="#"></a>
+            <div className="item">
+                <p className="title">{data.title}</p>
+                <p className="comment">{data.comment}</p>
+            </div>
+        </div>
+    ))
+
     return (
         <div className="main">
             <div className="category1">
-                <div className="category1_menu">
-                    <div className="window">
-                        <ul className="container">
-                            <li><img className="cimg selected" src="img/exercise15.jpg" /></li>
-                            <li><img className="cimg" src="img/exercise15.jpg" /></li>
-                            <li><img className="cimg" src="img/exercise18.jpg" /></li>
-                            <li><img className="cimg" src="img/exercise20.jpg" /></li>
-                        </ul>
-                    </div>
-                    <ul className="buttons">
-                        <li><div className="button selected"></div></li>
-                        <li><div className="button"></div></li>
-                        <li><div className="button"></div></li>
-                        <li><div className="button"></div></li>
-                    </ul>
-                </div>
+                <Slider {...settings}>
+                    <img className="cimg" src="img/exercise15.jpg" />
+                    <img className="cimg" src="img/exercise18.jpg" />
+                    <img className="cimg" src="img/exercise20.jpg" />
+                </Slider>
             </div>
             <br />
 
             <div className="category2">
                 <div className="category2_title"><h3>운동 루틴 추천</h3></div>
                 <div className="category2_item">
-                    <div className="items">
-                        <a href="#"></a>
-                        <div className="item">
-                            <p className="title">어깨 루틴</p>
-                            <p className="comment">레터럴레이즈, 숄더프레스 ...</p>
-                        </div>
-                    </div>
-                    <div className="items">
-                        <a href="#"></a>
-                        <div className="item">
-                            <p className="title">어깨 루틴</p>
-                            <p className="comment">레터럴레이즈, 숄더프레스 ...</p>
-                        </div>
-                    </div>
-                    <div className="items">
-                        <a href="#"></a>
-                        <div className="item">
-                            <p className="title">어깨 루틴</p>
-                            <p className="comment">레터럴레이즈, 숄더프레스 ...</p>
-                        </div>
-                    </div>
-                    <div className="items">
-                        <a href="#"></a>
-                        <div className="item">
-                            <p className="title">어깨 루틴</p>
-                            <p className="comment">레터럴레이즈, 숄더프레스 ...</p>
-                        </div>
-                    </div>
+                    {workoutList}
                 </div>
 
                 <div className="category2_title"><h3>식단 추천</h3></div>
                 <div className="category2_item">
-                    <div className="items">
-                        <a href="#"></a>
-                        <div className="item">
-                            <p className="title">아침</p>
-                            <p className="comment">레터럴레이즈, 숄더프레스 ...</p>
-                        </div>
-                    </div>
-                    <div className="items">
-                        <a href="#"></a>
-                        <div className="item">
-                            <p className="title">점심</p>
-                            <p className="comment">레터럴레이즈, 숄더프레스 ...</p>
-                        </div>
-                    </div>
-                    <div className="items">
-                        <a href="#"></a>
-                        <div className="item">
-                            <p className="title">저녁</p>
-                            <p className="comment">레터럴레이즈, 숄더프레스 ...</p>
-                        </div>
-                    </div>
-                    <div className="items">
-                        <a href="#"></a>
-                        <div className="item">
-                            <p className="title">간식</p>
-                            <p className="comment">레터럴레이즈, 숄더프레스 ...</p>
-                        </div>
-                    </div>
+                    {dietList}
                 </div>
             </div>
         </div >
