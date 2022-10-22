c3b77eb622e9e41763d11f97d33bc15e3ea1d1d5
jeonchaerin <chaerin0411@naver.com>
2022-10-22 23:52:06 +0900

    feat: 로그인 시 헤더에 아이디 띄우게 수정

--- client/src/components/layouts/Header.js
+++ client/src/components/layouts/Header.js
@@ -1,30 +1,24 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { Navbar, Nav, Button, Container, Dropdown } from "react-bootstrap";
 import SignUpModal from "../modals/SignUpModal";
 import SignInModal from "../modals/SignInModal";
 import Notification from "../../pages/notification/Notification";
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-// import NotificationsIcon from '@mui/icons-material/Notifications';
-// import { Routes, Route, Link } from "react-router-dom";
+
 
 const Header = () => {
     const [signUpModalOn, setSignUpModalOn] = useState(false);
     const [signInModalOn, setSignInModalOn] = useState(false);
     const [notificationOn, setNotificationOn] = useState(false);
 
-    // const logIn = () => {
-    //     if (sessionStorage.getItem('id') != null) {
-    //         return logRender = <p>환영합니다 {sessionStorage.getItem('id')}님 !</p>
-    //     } else {
-    //         return logRender = null;
-    //     }
-    // }
+    const id = sessionStorage.getItem('id');
 
     const logOut = () => {
         const logout = window.confirm("로그아웃 하시겠습니까?");
         if (logout === true) {
             sessionStorage.clear();
+            document.location.href = '/';
         } else {
             document.location.href = '/';
         }
@@ -57,33 +51,39 @@ const Header = () => {
                                 <Nav.Link href="/QnAList">QnA</Nav.Link>
                                 <Nav.Link href="/trainerWrite">내정보</Nav.Link>
                             </Nav>
-                            <Nav>
-                                <Nav.Link>
-                                    <Button variant="outline-dark" className="rounded-pill" onClick={() => setSignInModalOn(true)}>
-                                        로그인
-                                    </Button>
-                                </Nav.Link>
-                                <Nav.Link>
-                                    <Button variant="outline-dark" className="rounded-pill" onClick={() => setSignUpModalOn(true)}>
-                                        회원가입
-                                    </Button>
-                                </Nav.Link>
-                                <Nav.Link>
-                                    <Dropdown>
-                                        <Dropdown.Toggle variant="dark" className="rounded-pill">
-                                            <MenuIcon />
-                                            <AccountCircleIcon />
-                                        </Dropdown.Toggle>
+                            {id ? (
+                                <Nav>
+                                    <Nav.Link>
+                                        {id} 님
+                                    </Nav.Link>
+                                    <Nav.Link>
+                                        <Dropdown>
+                                            <Dropdown.Toggle variant="dark" className="rounded-pill">
+                                                <MenuIcon />
+                                                <AccountCircleIcon />
+                                            </Dropdown.Toggle>
 
-                                        <Dropdown.Menu>
-                                            <Dropdown.Item href="#" onClick={() => setSignInModalOn(true)}>로그인</Dropdown.Item>
-                                            <Dropdown.Item href="#" onClick={() => setSignUpModalOn(true)}>회원가입</Dropdown.Item>
-                                            <Dropdown.Item href="#" onClick={() => setNotificationOn(true)}>알림</Dropdown.Item>
-                                            <Dropdown.Item href="#" onClick={() => logOut()}>로그아웃</Dropdown.Item>
-                                        </Dropdown.Menu>
-                                    </Dropdown>
-                                </Nav.Link>
-                            </Nav>
+                                            <Dropdown.Menu>
+                                                <Dropdown.Item href="#" onClick={() => setNotificationOn(true)}>알림</Dropdown.Item>
+                                                <Dropdown.Item href="#" onClick={() => logOut()}>로그아웃</Dropdown.Item>
+                                            </Dropdown.Menu>
+                                        </Dropdown>
+                                    </Nav.Link>
+                                </Nav>
+                            ) : (
+                                <Nav>
+                                    <Nav.Link>
+                                        <Button variant="outline-dark" className="rounded-pill" onClick={() => setSignInModalOn(true)}>
+                                            로그인
+                                        </Button>
+                                    </Nav.Link>
+                                    <Nav.Link>
+                                        <Button variant="outline-dark" className="rounded-pill" onClick={() => setSignUpModalOn(true)}>
+                                            회원가입
+                                        </Button>
+                                    </Nav.Link>
+                                </Nav>
+                            )}
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
