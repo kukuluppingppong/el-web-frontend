0676c59b9d5e4bcfd527ced8fa767f008237c921
jeonchaerin <chaerin0411@naver.com>
2022-10-24 23:16:09 +0900

    feat: Main.js에서 운동 및 식단 루틴 클릭 시 유튜브 영상 보여주는 모달추가

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Slider from "react-slick"
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import YoutubeModal from '../components/modals/YoutubeModal';
 import data from '../data/data.json'
 
 
 const Main = () => {
+    const [youtubeModalOn, setYoutubeModalOn] = useState(false);
+    const [info, setInfo] = useState({
+        title: '',
+        url: '',
+    })
+
+    const youtubeLoad = (data) => {
+        const info = {
+            title: data.title,
+            url: data.url,
+        };
+        setInfo(info);
+        setYoutubeModalOn(true);
+    };
 
     const settings = {
         dots: true, // 캐러셀이미지가 몇번째인지 알려주는 점을 보여줄지 정한다.
@@ -16,9 +31,9 @@ const Main = () => {
     };
 
     const workoutList = data.workoutList.map((data, index) => (
-        <div key={index} className="items">
+        <div div key={index} className="items" >
             <a href="#"></a>
-            <div className="item">
+            <div className="item" onClick={() => youtubeLoad(data)}>
                 <p className="title">{data.title}</p>
                 <p className="comment">{data.comment}</p>
             </div>
@@ -37,6 +52,12 @@ const Main = () => {
 
     return (
         <div className="main">
+            <YoutubeModal
+                info={info}
+                show={youtubeModalOn}
+                onHide={() => setYoutubeModalOn(false)}
+            />
+
             <div className="category1">
                 <Slider {...settings}>
                     <img className="cimg" src="img/exercise15.jpg" />
@@ -45,7 +66,6 @@ const Main = () => {
                 </Slider>
             </div>
             <br />
-
             <div className="category2">
                 <div className="category2_title"><h3>운동 루틴 추천</h3></div>
                 <div className="category2_item">
