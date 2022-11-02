ff50f2b7226103f2e6ebaa082b31c75a4c0b160e
jeonchaerin <chaerin0411@naver.com>
2022-11-02 21:31:53 +0900

    docs: /redux/features 폴더 숨김

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './features/counter/counterSlice';
-// import postsReducer from './features/posts/postsSlice';
-// import usersReducer from './features/users/usersSlice';
 
 
 const store = configureStore({
     reducer: {
-        counter: counterReducer,
-        // posts: postsReducer,
-        // users: usersReducer,
     }
 });
 
