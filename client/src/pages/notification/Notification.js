f1c396dd2424c678c1fe431076cb6e6244061f6d
jeonchaerin <chaerin0411@naver.com>
2022-11-07 15:42:38 +0900

    fix: Notification.js에 loadMemberList 함수 추가

--- client/src/pages/notification/Notification.js
+++ client/src/pages/notification/Notification.js
@@ -1,9 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Modal, Container } from "react-bootstrap";
 
 
 const Notification = ({ show, onHide }) => {
+    const [loadMemberList, setLoadMemberList] = useState([]);
+
+    const resMemberList = async () => {
+        const loadMemberList = await axios.get('/api/memberList');
+        console.log(loadMemberList.data);
+        setLoadMemberList(loadMemberList.data);
+    }
+
+    useEffect(() => {
+        resMemberList()
+    }, [])
+
+    const list = loadMemberList.map(data => (
+        <Modal.Body key={data.id}>
+            <p><a href={`/feedbackWorkout/${data.id}`} className="link">{data.name}님이 피드백 운동을 등록했습니다.<br /><small>{data.id * 5} mins ago</small></a></p>
+            <p><a href={`/feedbackDiet/${data.id}`} className="link">{data.name}님이 피드백 식단을 등록했습니다.<br /><small>{data.id * 5} mins ago</small></a></p>
+        </Modal.Body>
+
+    ))
 
     return (
         <Modal
@@ -17,10 +36,7 @@ const Notification = ({ show, onHide }) => {
                 <Modal.Header closeButton>
                     <Modal.Title id="contained-modal-title-vcenter">모든알림</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
-                    <p><Link to={"/feedbackWorkout/1"}>김회원님이 피드백 운동을 등록했습니다.<br /><small>1 mins ago</small></Link></p>
-                    <p><Link to={"/feedbackDiet/1"}>김회원님이 피드백 식단을 등록했습니다.<br /><small>1 mins ago</small></Link></p>
-                </Modal.Body>
+                {list}
             </Container>
         </Modal >
     )
